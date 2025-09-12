Fixes #1: (fixes commit messages on previous PR) Hessian of layers + loss landscape

modadd_grok.py
@@ -18,6 +18,9 @@ from datetime import datetime
 import hashlib
 import json
 
+from pyhessian import hessian
+import copy
+
 from model import params_pad_to_shape, Transformer, GradientLogger
 import itertools
 
@@ -48,7 +51,7 @@ def full_loss(model, data, device):
     x = x.to(device)
     labels = labels.to(device)
     logits, attn_scores_masked = model(x)
-    logits = logits[:, -1]
+    # logits = logits[:, -1]
     return torch.nn.functional.cross_entropy(logits, labels), attn_scores_masked
 
 def full_accuracy(model, data, device):
@@ -57,10 +60,14 @@ def full_accuracy(model, data, device):
     x, labels = next(iter(loader))
     x = x.to(device)
     labels = labels.to(device)
-    logits = model(x)[0][:, -1]
+    logits = model(x)[0] #[:, -1]
     predictions = torch.argmax(logits, dim=1)
     return torch.sum(predictions == labels).item() / len(labels)
 
+def get_params(model_orig, model_perb, direction, alpha):
+    for m_orig, m_perb, d in zip(model_orig.parameters(), model_perb.parameters(), direction):
+        m_perb.data = m_orig.data + alpha * d
+    return model_perb
 
 def train_and_plot(sizes,
                    train,
@@ -68,7 +75,8 @@ def train_and_plot(sizes,
                    t_steps=5000,
                    exp_freq=None,
                    exp_thres=0.2,
-                   log_dir="./logs/"):
+                   log_dir="./logs/", 
+                   seed=42):
     log_steps = []
     train_losses = []
     test_losses = []
@@ -77,7 +85,7 @@ def train_and_plot(sizes,
     norms = []
     grad_norms = []
     attention_scores = []
-
+    top_hessian_eigenvalues = []
     # if not exp_freq:
     #     exp_freq = t_steps // len(sizes)
     size_index = 0
@@ -167,6 +175,45 @@ def train_and_plot(sizes,
                                        dim=0)  # Average across layers
                 mean_attn = mean_attn.mean(dim=0)  # Average across batches
                 attention_scores.append(mean_attn.cpu().numpy())
+                
+            criterion = torch.nn.functional.cross_entropy
+            train_loader = torch.utils.data.DataLoader(train, batch_size=len(train), shuffle=False)
+                        
+            hessian_comp = hessian(model=model, criterion=criterion, cuda=True, dataloader=train_loader)
+
+            top_eigenvalues, top_eigenvector = hessian_comp.eigenvalues()
+
+            # calculating top eigenvalues
+            eigenvalues_np = np.array(top_eigenvalues)
+
+            top_hessian_eigenvalues.append(eigenvalues_np)
+
+            lams = np.linspace(-0.5, 0.5, 21).astype(np.float32)
+
+            loss_list = []
+
+            # computing and saving loss landscape plots
+            model_perb = copy.deepcopy(model).to(device)
+            for lam in lams:
+                model_perb = get_params(model, model_perb, top_eigenvector[0], lam)
+                loss_val, _ = full_loss(model_perb, train, device)
+                loss_list.append(loss_val.item())
+            # exp_name = f"{now}__{tag}__{short_hash}"
+            
+            exp_name = " | ".join(
+                ["_".join(f"{v}" for k, v in vars(s).items())
+                for s in sizes]) + f"@step{exp_freq}"
+            exp_path = os.path.join(log_dir, exp_name, f"seed_{seed}")
+            
+            
+            plt.plot(lams, loss_list)
+            plt.xlabel("Perturbation")
+            plt.ylabel("Loss")
+            plt.title(f"Epoch {epoch} loss landscape")
+            loss_land_dir = os.path.join(exp_path, "loss_landscapes")
+            os.makedirs(loss_land_dir, exist_ok=True)
+            plt.savefig(os.path.join(loss_land_dir,f"landscape_epoch{epoch}.png"))
+            plt.close()
 
         train_loss.backward()
 
@@ -188,9 +235,13 @@ def train_and_plot(sizes,
         optimizer.step()
         optimizer.zero_grad()
 
-    grad_logger.save("grads.pt")
+    grad_logger.save(path=os.path.join(exp_path, "grads.pt"))
     grad_logger.plot_grad_norms()
     grad_logger.plot_grad_hist()
+
+    # save hessian eigenvalue array
+    np.save(os.path.join(exp_path, "hessian_eigenvalues"), 
+            np.array(top_hessian_eigenvalues))
     
     # Pad attention scores to match largest shape before storing
     if attention_scores:
@@ -246,50 +297,54 @@ def train_and_plot(sizes,
     with open(os.path.join(exp_path, "config.json"), "w") as f:
         json.dump(data_json, f, indent=2)
 
-    # # Create figure with subplots
-    # fig, (ax1, ax2) = plt.subplots(2,
-    #                                1,
-    #                                figsize=(10, 12),
-    #                                height_ratios=[2, 1])
+    # Create figure with subplots
+    fig, (ax1, ax2) = plt.subplots(2,
+                                   1,
+                                   figsize=(10, 12),
+                                   height_ratios=[2, 1])
 
-    # # Plot accuracies and norms
-    # expansion_steps = [exp_freq * i for i in range(1, len(sizes))]
+    # Plot accuracies and norms
+    expansion_steps = [exp_freq * i for i in range(1, len(sizes))]
 
-    # ax1.plot(log_steps, train_accuracies, color='red', label='train')
-    # ax1.plot(log_steps, test_accuracies, color='green', label='test')
+    ax1.plot(log_steps, train_accuracies, color='red', label='train')
+    ax1.plot(log_steps, test_accuracies, color='green', label='test')
 
-    # if any(a >= 0.95 for a in test_accuracies):
-    #     time_to_95_pct_test = log_steps[min(
-    #         i for i, acc in enumerate(test_accuracies) if acc >= 0.95)]
-    #     ax1.plot([time_to_95_pct_test] * 2, [0, 1],
-    #              color='green',
-    #              linestyle='--')
-    #     ax1.text(time_to_95_pct_test + 10, 0.65,
-    #              f"@{time_to_95_pct_test} test acc\nhits 95%")
+    if any(a >= 0.95 for a in test_accuracies):
+        time_to_95_pct_test = log_steps[min(
+            i for i, acc in enumerate(test_accuracies) if acc >= 0.95)]
+        ax1.plot([time_to_95_pct_test] * 2, [0, 1],
+                 color='green',
+                 linestyle='--')
+        ax1.text(time_to_95_pct_test + 10, 0.65,
+                 f"@{time_to_95_pct_test} test acc\nhits 95%")
 
-    # for step in expansion_steps:
-    #     ax1.axvline(x=step,
-    #                 color='blue',
-    #                 linestyle='dotted',
-    #                 linewidth=1,
-    #                 alpha=0.2)
+    for step in expansion_steps:
+        ax1.axvline(x=step,
+                    color='blue',
+                    linestyle='dotted',
+                    linewidth=1,
+                    alpha=0.2)
 
-    # ax1.legend()
-    # ax1.set_xlabel("Optimization Steps")
-    # ax1.set_ylim(0, 1)
-    # ax1.set_ylabel("Accuracy")
+    ax1.legend()
+    ax1.set_xlabel("Optimization Steps")
+    ax1.set_ylim(0, 1)
+    ax1.set_ylabel("Accuracy")
 
-    # ax1_2 = ax1.twinx()
-    # ax1_2.set_ylabel("Weight/Grad Norm", color='purple')
-    # ax1_2.plot(log_steps, norms, color='purple', label='weight norm')
-    # ax1_2.plot(log_steps, grad_norms, color='orange', label='grad norm')
-    # ax1_2.set_ylim(27, 63)
-    # ax1.set_xscale('log')
-    # ax1_2.legend(loc=(0.015, 0.72))
+    ax1_2 = ax1.twinx()
+    ax1_2.set_ylabel("Weight/Grad Norm", color='purple')
+    ax1_2.plot(log_steps, norms, color='purple', label='weight norm')
+    ax1_2.plot(log_steps, grad_norms, color='orange', label='grad norm')
+    ax1_2.set_ylim(27, 63)
+    ax1.set_xscale('log')
+    ax1_2.legend(loc=(0.015, 0.72))
 
     # # Plot attention heatmap
     # attention_scores = np.array(attention_scores) # shape b,i,q,h
-    # sns.heatmap(attention_scores.T,
+
+    # scores_2d = attention_scores.mean(axis=(0,3)) # average over batch and heads: result shape (i, q)
+
+    # sns.heatmap( # attention_scores.T, 
+    #             scores_2d,
     #             ax=ax2,
     #             cmap='viridis',
     #             xticklabels=log_steps[::len(log_steps) // 10],
@@ -345,8 +400,8 @@ if __name__ == "__main__":
         exp_thresholds = [0.1] # [0.1, 0.25, 0.4]
 
         # log_dir = f"log/modadd/base/seed_{seed}"
-        log_dir = os.path.join("log", "modadd", "base", f"seed_{seed}")
-        os.makedirs(log_dir, exist_ok=True)
+        # log_dir = os.path.join("log", "modadd", "base", f"seed_{seed}")
+        # os.makedirs(log_dir, exist_ok=True)
 
         # for size in base_sizes + target_sizes:
         #     sizes = [ModelSpec(size[0], size[1], size[2])]
@@ -358,9 +413,9 @@ if __name__ == "__main__":
             sizes = [ModelSpec(base[0], base[1], base[2]), ModelSpec(target[0], target[1], target[2])]
 
             # exp_name = " | ".join(["_".join(f"{v}" for k, v in vars(s).items()) for s in sizes])
-            log_dir = os.path.join("log", "modadd", "exp",f"thres_{et}" ,f"seed_{seed}")
+            log_dir = os.path.join("log", "modadd", "exp",f"thres_{et}")
             os.makedirs(log_dir, exist_ok=True)
 
             print(f"Training {sizes}")
 
-            train_and_plot(sizes, train, test, 5_000, -1, log_dir=log_dir,exp_thres=et)
+            train_and_plot(sizes, train, test, 5_000, -1, log_dir=log_dir,exp_thres=et, seed=seed)

model.py
@@ -65,7 +65,7 @@ class GradientLogger:
                 param.register_hook(lambda grad, n=name: self.grads[n].append(
                     grad.detach().cpu().clone()))
 
-    def save(self, path="grads.pt"):
+    def save(self, path):
         torch.save(dict(self.grads), path)
 
     def load(self, path="grads.pt"):
@@ -256,7 +256,7 @@ class Transformer(nn.Module):
 
         # x = self.ln(x)
         x = self.unembed(x)
-        return x, attn_scores_masked
+        return x[:,-1], attn_scores_masked
 
     def set_use_cache(self, use_cache):
         self.use_cache = use_cache